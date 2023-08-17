34f03687750ccbe75a1754af610b7fe942a6d34e
Traxx <fabian.salzwedel@gmail.com>
2023-08-17 15:25:15 +0200

    migrated colors to separate file

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,14 +11,7 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 #include "Helpers/FFTDataGenerator.h"
-
-inline juce::Colour COLOR_BG = juce::Colour(50u, 50u, 50u);
-inline juce::Colour COLOR_BG_VERYDARK = juce::Colour(18u, 18u, 18u);
-inline juce::Colour COLOR_BG_DARK = juce::Colour(25u, 25u, 25u);
-inline juce::Colour COLOR_BG_MIDDARK = juce::Colour(36u, 36u, 36u);
-inline juce::Colour COLOR_BG_MID = juce::Colour(64u, 64u, 64u);
-inline juce::Colour COLOR_BG_LIGHT = juce::Colour(86u, 86u, 86u);
-inline juce::Colour COLOR_KNOB = juce::Colour(110u, 10u, 10u);
+#include "UI/Colors.h"
 
 struct LookAndFeelSliderValues : juce::LookAndFeel_V4 {
     void drawRotarySlider(juce::Graphics& g,
---
+++ Source/UI/Colors.h
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    Colors.h
+    Created: 17 Aug 2023 3:20:47pm
+    Author:  traxx
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+inline juce::Colour COLOR_BG = juce::Colour(50u, 50u, 50u);
+inline juce::Colour COLOR_BG_VERYDARK = juce::Colour(18u, 18u, 18u);
+inline juce::Colour COLOR_BG_DARK = juce::Colour(25u, 25u, 25u);
+inline juce::Colour COLOR_BG_MIDDARK = juce::Colour(36u, 36u, 36u);
+inline juce::Colour COLOR_BG_MID = juce::Colour(64u, 64u, 64u);
+inline juce::Colour COLOR_BG_LIGHT = juce::Colour(86u, 86u, 86u);
+inline juce::Colour COLOR_KNOB = juce::Colour(110u, 10u, 10u);
