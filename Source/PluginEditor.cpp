83fbb07e888569f578bb003a09908f111a884cbd
Traxx <fabian.salzwedel@gmail.com>
2023-10-04 14:20:19 +0200

    feat: add automatic check for update

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -93,6 +93,7 @@ GnomeDistortAudioProcessorEditor::GnomeDistortAudioProcessorEditor(GnomeDistortA
     };
 
     setSize(420, 600);
+    checkForUpdates();
 }
 
 //==============================================================================
@@ -352,6 +353,52 @@ void GnomeDistortAudioProcessorEditor::resized() {
     WaveShapeAmountSlider.setBounds(bounds);
 
     paintBackground();
+}
+
+void GnomeDistortAudioProcessorEditor::checkForUpdates() {
+    using namespace juce;
+    const URL url = URL("https://api.github.com/repos/crowbait/GnomeDistort/releases/latest");
+    StringPairArray responseHeaders;
+    int statusCode = 0;
+    String response;
+
+    File appdir = File::getSpecialLocation(File::SpecialLocationType::userDocumentsDirectory);
+    File lastCheckFile = File(appdir.getFullPathName() + File::getSeparatorString() + "CrowbaitVSTs" + File::getSeparatorString() + "GnomeDistort-update-check");
+    if (!lastCheckFile.exists()) lastCheckFile.create();
+    StringArray readLines;
+    lastCheckFile.readLines(readLines);
+    DBG("Last check: " + readLines[0]);
+
+    if (readLines[0] == "" || Time::fromISO8601(readLines[0]).getDayOfMonth() != Time().getCurrentTime().getDayOfMonth()) {
+        DBG("Checking for update");
+        lastCheckFile.replaceWithText(Time::getCurrentTime().toISO8601(true));
+        std::unique_ptr<InputStream> stream(url.createInputStream(false, nullptr, nullptr, String(),
+                                                                  750, // timeout in ms
+                                                                  &responseHeaders, &statusCode));
+        if (stream != nullptr) {
+            response = stream->readEntireStreamAsString();
+            DBG(response);
+            var json = JSON::parse(response);
+            String versionString = json.getProperty("tag_name", "Error").toString();
+            if (versionString != ProjectInfo::versionString) {
+                AlertWindow::showAsync(
+                    MessageBoxOptions()
+                    .makeOptionsOkCancel(MessageBoxIconType::InfoIcon,
+                                         "Update available",
+                                         "A new update is available: " + versionString + newLine +
+                                         "You have version " + ProjectInfo::versionString + " installed." + newLine,
+                                         "Download", "Maybe later"),
+                    [](int res) { // res: 1 = Download, 0 = Maybe later
+                    if (res == 1) URL("https://github.com/crowbait/GnomeDistort/releases").launchInDefaultBrowser();
+                });
+            } else DBG("No update available");
+            return;
+        }
+        if (statusCode != 0 && statusCode != 200) {
+            DBG("Failed to connect, status " + String(statusCode));
+        } else DBG("Failed to connect.");
+    } else DBG("Already checked for update today.");
+    lastCheckFile.~File();
 }
 
 std::vector<juce::Component*> GnomeDistortAudioProcessorEditor::getComponents() {
